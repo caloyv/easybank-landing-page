1ec044c6a5361b6383b404cd40496d5bbe161b95
caloyv <villarta.carlos.b.7725@gmail.com>
2024-03-10 04:23:21 +0800

    major refactor and transition fix

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
+const gray = document.querySelector(".gray");
+const menuBtn = document.querySelector(".menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+const menu = document.querySelector(".mobile-menu-container");
 
-const gray = document.querySelector('.gray')
-const menuBtn = document.querySelector('.menu-btn')
-const closeBtn = document.querySelector('.close-btn')
-let menu = document.querySelector('.mobile-menu-container')
+menuBtn.addEventListener("click", () => {
+    removeActive();
+    closeBtn.classList.add("active");
+    menu.classList.add("active");
+    gray.classList.remove("hidden");
+    document.body.classList.add("scroll-lock");
+});
+closeBtn.addEventListener("click", () => {
+    removeActive();
+    menuBtn.classList.add("active");
+    gray.classList.add("hidden");
+});
 
-menuBtn.addEventListener('click', () => {
-    console.log("working")
-    menuBtn.classList.remove('active')
-    closeBtn.classList.add('active')
-    menu.classList.add('active')
-    gray.classList.remove('hidden')
-})
-closeBtn.addEventListener('click', () => {
-    console.log("working2")
-    menuBtn.classList.add('active')
-    closeBtn.classList.remove('active')
-    menu.classList.remove('active')
-    gray.classList.add('hidden')
-})
+gray.addEventListener("click", () => {
+    removeActive();
+    menuBtn.classList.add("active");
+    gray.classList.add("hidden");
+});
 
-gray.addEventListener('click', () => {
-    menuBtn.classList.add('active')
-    closeBtn.classList.remove('active')
-    menu.classList.remove('active')
-    gray.classList.add('hidden')
-})
-
-    
+const removeActive = () => {
+    menuBtn.classList.remove("active");
+    closeBtn.classList.remove("active");
+    menu.classList.remove("active");
+    document.body.classList.remove("scroll-lock");
+};
